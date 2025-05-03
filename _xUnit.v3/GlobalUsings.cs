da0b75bf2250a646fff332d9a0f9d97d45cab671
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-05-03 18:55:26 +0200

    license headings

--- _xUnit.v3/GlobalUsings.cs
+++ _xUnit.v3/GlobalUsings.cs
@@ -1,4 +1,7 @@
-﻿global using CsabaDu.DynamicTestData.DynamicDataSources;
+﻿// SPDX-License-Identifier: MIT
+// Copyright (c) 2025. Csaba Dudas (CsabaDu)
+
+global using CsabaDu.DynamicTestData.DynamicDataSources;
 global using CsabaDu.DynamicTestData.SampleCodes.xUnit.v3.DynamicDataSources;
 global using CsabaDu.DynamicTestData.TestDataTypes;
 global using CsabaDu.DynamicTestData.xUnit.v3.Attributes;
