719b0afd097c6896dd161d99516d38ade1c7dc03
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-06-27 17:32:51 +0200

    TestCaseTestData tests completed

--- _NUnit/UnitTests/BirthdayTests_NUnit_TestCaseTestData.cs
+++ _NUnit/UnitTests/BirthdayTests_NUnit_TestCaseTestData.cs
@@ -94,9 +94,85 @@ public class BirthdayTests_NUnit_TestCaseTestData
         BirthDay? other = testData.Arg2;
         BirthDay sut = new(name, dateOfBirth);
 
-        // Act
+        // Act & Assert
         return sut.CompareTo(other);
     }
     #endregion
+
+    #region ArgsCode.Properties sample tests
+    #region Constructor tests
+    private static IEnumerable<TestCaseTestData>? BirthDayConstructorValidArgs_Props
+    => DataSource.GetBirthDayConstructorValidArgs(nameof(Ctor_validArgs_createsInstance_Props), ArgsCode.Properties);
+
+    [TestCaseSource(nameof(BirthDayConstructorValidArgs_Props))]
+    public void Ctor_validArgs_createsInstance_Props(
+        DateOnly dateOfBirth)
+    {
+        // Arrange
+        string name = "valid name";
+
+        // Act
+        var actual = new BirthDay(name, dateOfBirth);
+
+        // Assert
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual.Name, Is.EqualTo(name));
+            Assert.That(actual.DateOfBirth, Is.EqualTo(dateOfBirth));
+        }
+    }
+
+    private static IEnumerable<TestCaseTestData>? BirthDayConstructorInvalidArgs_Props
+    => DataSource.GetBirthDayConstructorInvalidArgs(nameof(Ctor_invalidArgs_throwsArgumentException_Props), ArgsCode.Properties);
+
+    [TestCaseSource(nameof(BirthDayConstructorInvalidArgs_Props))]
+    public void Ctor_invalidArgs_throwsArgumentException_Props(
+        ArgumentException expected,
+        string? name)
+    {
+        // Arrange
+        DateOnly dateOfBirth = DateOnly.FromDateTime(DateTime.Now).AddDays(1);
+        void attempt() => _ = new BirthDay(name!, dateOfBirth);
+
+        // Act & Assert
+        try
+        {
+            attempt();
+            Assert.Fail(
+                $"Expected {expected.GetType().Name} was not thrown.");
+        }
+        catch (ArgumentException actual)
+        {
+            Assert.That(actual, Is.TypeOf(expected.GetType()));
+            Assert.That(actual?.Message, Is.EqualTo(expected.Message));
+            Assert.That(actual?.ParamName, Is.EqualTo(expected.ParamName));
+        }
+        catch (Exception ex)
+        {
+            Assert.Fail(
+                $"Unexpected exception type: {ex.GetType().Name}");
+        }
+    }
+    #endregion
+
+    #region CompareTo tests
+    private static IEnumerable<TestCaseTestData>? CompareToArgs_Props
+        => DataSource.GetCompareToArgs( nameof(CompareTo_validArgs_returnsExpected_Props), ArgsCode.Properties);
+
+    [TestCaseSource(nameof(CompareToArgs_Props))]
+    public int CompareTo_validArgs_returnsExpected_Props(
+        DateOnly dateOfBirth,
+        BirthDay? other)
+    {
+        // Arrange
+        string name = "valid name";
+        BirthDay sut = new(name, dateOfBirth);
+
+        // Act & Assert
+        return sut.CompareTo(other);
+    }
+    #endregion
+    #endregion ArgsCode.Properties sample tests
 
 }
